b996d1747a1b9882081c5f9b573d6d355e110832
tharvik <tharvik@users.noreply.github.com>
2022-11-22 18:48:08 +0100

    test/ebpf: rework

--- test_programs/ebpf/return_if.c
+++ test_programs/ebpf/return_if.c
@@ -1,6 +1,6 @@
 int main()
 {
-    int a = 1; 
+    int a = 42; 
     if (a) {
       return a;
     } else {
--- tests/test_ebpf.py
+++ tests/test_ebpf.py
@@ -1,48 +1,31 @@
 import logging
-import os
+from pathlib import Path
 
 import angr
-from archinfo import Endness
-from angr_platforms.ebpf import ArchEbpf, SimOsEbpf
+from angr_platforms.ebpf import ArchExtendedBPF
 
+# TODO debug
 logging.root.setLevel(logging.DEBUG)
 
-TEST_PROGRAMS_BASE = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'test_programs', 'ebpf')
+TEST_PROGRAMS_BASE = Path(__file__).parent.parent / "test_programs" / "ebpf"
 
+def test_prog_always_returns_42(filename: str) -> None:
+    proj = angr.Project(TEST_PROGRAMS_BASE / filename)
+    assert isinstance(proj.arch, ArchExtendedBPF)
 
-def print_(s, indent=0):
-    """
-    printing function with indentation
-    """
-    print('\t' * indent + str(s))
+    solver = proj.factory.entry_state().solver
+    simgr = proj.factory.simulation_manager()
+    simgr.explore()
 
+    assert len(simgr.deadended) == 1
+    assert solver.eval_exact(simgr.deadended[0].regs.R0, 1) == [42]
 
-def create_program(program_name):
-    proj = angr.Project(os.path.join(TEST_PROGRAMS_BASE, program_name), arch=ArchEbpf(Endness.LE), simos=SimOsEbpf)
-    state = proj.factory.entry_state()
-    state.regs.R1 = 1
-    simgr = proj.factory.simgr(state)  # type:angr.SimulationManager
-    simgr.run()
+def test_trivial_return():
+    test_prog_always_returns_42('return_42.o')
 
-    print(f'Result of running {program_name}')
-    print(simgr)
-    for state in simgr.deadended:
-        print_(f'state {state} constraints:', indent=1)
-        constraints = state.solver.constraints
-        if len(constraints) > 0:
-            for constraint in state.solver.constraints:
-                print_(constraint, indent=2)
-        else:
-            print_('[]', indent=2)
+def test_branched_return():
+    test_prog_always_returns_42('return_if.o')
 
-
-test_programs_to_run = [
-    'return_42.o',
-    'return_42_long.o',
-    'return_if.o',
-    'dynamic_return.o'
-]
-
-for prog_name in test_programs_to_run:
-    print(f"Running program {prog_name} :")
-    create_program(prog_name)
+if __name__ == "__main__":
+    test_trivial_return()
+    test_branched_return()
